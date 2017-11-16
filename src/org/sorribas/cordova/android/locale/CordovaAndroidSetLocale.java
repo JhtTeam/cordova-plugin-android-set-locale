29b4d0a92818223a24124f7c978fa305b650bec7
Eduardo Sorribas <eduardo@sorribas.org>
2017-11-16 16:06:04 +0100

    init.

---
+++ src/org/sorribas/cordova/android/locale/CordovaAndroidSetLocale.java
@@ -0,0 +1,25 @@
+package org.sorribas.cordova.android.locale;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class CordovaAndroidSetLocale extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("greet")) {
+
+            String name = data.getString(0);
+            String message = "Hello, " + name;
+            callbackContext.success(message);
+
+            return true;
+
+        } else {
+            
+            return false;
+
+        }
+    }
+}
