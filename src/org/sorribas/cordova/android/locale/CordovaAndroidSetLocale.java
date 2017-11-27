7125762b9b79841096e186fdaf229a21873dc5b3
Eduardo Sorribas <eduardo@sorribas.org>
2017-11-27 13:11:01 +0100

    First working version.

--- src/org/sorribas/cordova/android/locale/CordovaAndroidSetLocale.java
+++ src/org/sorribas/cordova/android/locale/CordovaAndroidSetLocale.java
@@ -1,25 +1,56 @@
 package org.sorribas.cordova.android.locale;
 
+import java.util.Locale;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Handler;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class CordovaAndroidSetLocale extends CordovaPlugin {
+  private Context context;
+  private android.app.Activity activity;
 
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("greet")) {
+  @Override
+  public void initialize(final CordovaInterface cordova, final CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    this.context = (Context) cordova.getActivity().getApplication();
+    this.activity = cordova.getActivity();
+  }
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
+  @Override
+  public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("setLocale")) {
+      Handler mainHandler = new Handler(context.getMainLooper());
 
-            return true;
+      final String lang = data.getString(0);
+      Runnable myRunnable = new Runnable() {
+        @Override 
+        public void run() {
+          try {
+            Class<?> activityManagerNative = Class.forName("android.app.ActivityManagerNative");                
+            Object am=activityManagerNative.getMethod("getDefault").invoke(activityManagerNative); 
+            Object config=am.getClass().getMethod("getConfiguration").invoke(am); 
+            config.getClass().getDeclaredField("locale").set(config, new Locale(lang)); 
+            config.getClass().getDeclaredField("userSetLocale").setBoolean(config, true); 
 
-        } else {
-            
-            return false;
+            am.getClass().getMethod("updateConfiguration", Configuration.class).invoke(am,config); 
 
+          } catch (Exception e) {
+
+          }
         }
+      };
+      mainHandler.post(myRunnable);
+      return true;
+
+    } else {
+
+      return false;
+
     }
+  }
 }
